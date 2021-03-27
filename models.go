8094a22e22cd4b4a987eccc2d733374277c5166d
Akin Ade <mrspence004@gmail.com>
2021-03-27 19:06:20 -0400

    .....

--- models.go
+++ models.go
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-package main
-
-import (
-	"time"
-)
-
-type Profile struct {
-	ID string 	`json:'ID'`
-	Name string `json:'name'`
-	Coins int 	`json:'coins'`
-	Organization bool `json:'organization'`
-}
-
-type Task struct {
-	ID string 						`json:'ID'`
-	CreatedBy string 				`json:'created_by'`
-	DateToComplete time.Time 		`json:'date_to_complete'`
-	TaskType string 				`json:'task_type'`
-	TimeToComplete time.Duration 	`json:'time_to_complete'`
-	Distance int 					`json:'distance'`
-	Reward int 						`json:'reward'`
-	Description string				`json:'description'`
-	Status int					    `json:'status'`
-}
-
-type TasksAccepted struct {
-	UID string 						`json:'uid'`
-	TaskID string 					`json:'task_id'`
-}
-
-=======
 package main
 
 import (
@@ -60,5 +28,4 @@ type TasksAccepted struct {
 	TaskID string 					`json:"task_id"`
 }
 
->>>>>>> f885bd224151c036be7b5059f3329baf22630706
 type GetTaskResponse []*Task
