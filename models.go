9b7f277a8b5a5e543b0071933c40a21667604d0b
AllenKaplan <allenkaplan07@gmail.com>
2021-03-28 02:33:59 -0400

    fixes2

--- models.go
+++ models.go
@@ -5,32 +5,28 @@ import (
 )
 
 type Profile struct {
-	ID string 	`json:"id"`
-	Name string `json:"name"`
-	Coins int 	`json:"coins"`
-	Organization bool `json:"organization"`
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	Coins        int    `json:"coins"`
+	Organization bool   `json:"organization"`
 }
-
 
 type Task struct {
-	ID int 						`json:"id"`
-	CreatedBy string 				`json:"created_by"`
-	DateToComplete time.Time 		`json:"date_to_complete"`
-	TaskType string 				`json:"task_type"`
-	TimeToComplete int 	`json:"time_to_complete"`
-	Lat float64 						`json:"lat"`
-	Long float64 				`json:"long"`
-	Reward int 						`json:"reward"`
-	Description string				`json:"description"`
-	Status int					    `json:"status"`
+	ID             int       `json:"id"`
+	CreatedBy      string    `json:"created_by"`
+	DateToComplete time.Time `json:"date_to_complete"`
+	TaskType       string    `json:"task_type"`
+	TimeToComplete int       `json:"time_to_complete"`
+	Lat            float64   `json:"lat"`
+	Long           float64   `json:"long"`
+	Reward         int       `json:"reward"`
+	Description    string    `json:"description"`
 }
-
 
 // type TaskResponse struct {
 // 	Task
 // 	Distance int 					`json:"distance"`
 // }
-
 
 // type TaskRequest struct {
 // 	Task
@@ -38,15 +34,14 @@ type Task struct {
 // 	Long float64 				`json:"long"`
 // }
 
-
 type TasksAccepted struct {
-	UID string 						`json:"uid"`
-	TaskID int 					`json:"task_id"`
+	UID    string `json:"uid"`
+	TaskID int    `json:"task_id"`
 }
 
 type GetTaskResponse []*Task
 
 type TaskCompleteRequest struct {
-	UID string 						`json:"uid"`
-	TaskID int 					`json:"task_id"`
+	UID    string `json:"uid"`
+	TaskID int    `json:"task_id"`
 }
